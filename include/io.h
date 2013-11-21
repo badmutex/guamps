788550ac9189c2a692d58533b1fe8427d1dc2eff
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2013-11-21 10:35:19 -0500

    use a more abstract gmx_data_t type

--- include/io.h
+++ include/io.h
@@ -9,6 +9,13 @@
 #include "gromacs/tpxio.h"
 #include "gromacs/checkpoint.h"
 
+typedef struct gmx_data_s {
+    enum type_e { RVEC, INT } type;
+    union data_u {
+        rvec *rvec;
+        int number;
+    } data;
+} gmx_data_t;
 
 typedef union selection_result_u {
     rvec *vec;
@@ -16,19 +23,14 @@ typedef union selection_result_u {
 } selection_result_t;
 
 
-int guamps_read_checkpoint_X(const char *path, const char *selector, selection_result_t *result);
-
-int guamps_read_tpr_X(const char *path, const char *selector, selection_result_t *result);
-int guamps_get_state_X(const t_state *state, const char *selector, selection_result_t *result);
-
-int guamps_get_state_coords(const t_state *state, rvec *vec);
-int guamps_get_state_(const t_state *state, rvec *vec);
-
-
+int guamps_read_checkpoint_X(const char *path, const char *selector, gmx_data_t *result);
+int guamps_get_state_X(const t_state *state, const char *selector, gmx_data_t *result);
 int guamps_write_rvec(FILE *fh, const rvec *vec, const int length);
 
-int guamps_read_checkpoint(const char *path, t_state *state);
+int guamps_write(FILE *fh, const gmx_data_t *data);
 
+int guamps_read_tpr_X(const char *path, const char *selector, selection_result_t *result);
+int guamps_read_checkpoint(const char *path, t_state *state);
 int guamps_read_tpr(const char *path, t_inputrec *params, t_state *state);
 
 
--- src/io.c
+++ src/io.c
@@ -1,6 +1,6 @@
 #include "io.h"
 
-int guamps_read_checkpoint_X(const char *path, const char *selector, selection_result_t *result) {
+int guamps_read_checkpoint_X(const char *path, const char *selector, gmx_data_t *result) {
 
   int part;
   gmx_large_int_t step;
@@ -13,21 +13,24 @@ int guamps_read_checkpoint_X(const char *path, const char *selector, selection_r
   return guamps_get_state_X(&state, selector, result);
 }
 
-int guamps_get_state_X(const t_state *state, const char *selector, selection_result_t *result) {
+int guamps_get_state_X(const t_state *state, const char *selector, gmx_data_t *result) {
 
   const char *sel = selector;
   const t_state *st = state;
 
   if      (strcmp(sel, "natoms") == 0) {
-    result->count = st->natoms;
+    result->type         = INT;
+    result->data.number = st->natoms;
     return 0;
   }
   else if (strcmp(sel, "position") == 0) {
-    result->vec = st->x;
+    result->type = RVEC;
+    result->data.rvec = st->x;
     return 0;
   }
   else if (strcmp(sel, "velocity") == 0) {
-    result->vec = st->v;
+    result->type = RVEC;
+    result->data.rvec = st->v;
     return 0;
   }
   else {
