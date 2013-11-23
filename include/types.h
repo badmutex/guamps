db0a66f794b3b0309c02c8ccb2ea5ca988d0ee8a
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2013-11-23 09:17:07 -0600

    whitespace change

--- include/types.h
+++ include/types.h
@@ -7,20 +7,21 @@
 
 
 typedef struct gmx_rvec_s {
-rvec *rvec;
-int  natoms;
+  rvec *rvec;
+  int  natoms;
 } gmx_rvec_t;
 
 typedef struct gmx_data_s {
-    enum type_e { RVEC, INT } type;
-    union data_u {
-        gmx_rvec_t vector;
-        int number;
-    } data;
+  enum type_e { RVEC, INT } type;
+  union data_u {
+    gmx_rvec_t vector;
+    int number;
+  } data;
 } gmx_data_t;
 
 typedef enum selector_e {
-    NATOMS, POSITIONS, VELOCITIES, FORCES, LAMBDA, BOX, STEP, TIME
+  NATOMS, POSITIONS, VELOCITIES, FORCES, LAMBDA, BOX, STEP, TIME,
+  RNG, RNG_IX
 } selector_t;
 
 typedef enum filetype_e {
