5ddf5e51b83d9f1a0748c1ac9ef28a57c6e41d02
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-02-24 10:01:38 -0500

    add NSTEPS selector

--- include/select.h
+++ include/select.h
@@ -5,6 +5,7 @@ typedef enum {
   NATOMS, POSITIONS, VELOCITIES,
   FORCES, LAMBDA, BOX, STEP, TIME,
   SEED, NSTLOG, NSTXOUT, NSTVOUT, NSTFOUT,
+  NSTEPS,
   selector_t_LAST
 } selector_t;
 static const int selector_t_count = selector_t_LAST - 1;
@@ -13,6 +14,7 @@ static const char *GUAMPS_SELECTOR_NAMES[] =
   {[NATOMS]="NATOMS", [POSITIONS]="POSITIONS", [VELOCITIES]="VELOCITIES",
    [FORCES]="FORCES", [LAMBDA]="LAMBDA", [BOX]="BOX", [STEP]="STEP", [TIME]="TIME",
    [SEED]="SEED", [NSTLOG]="NSTLOG", [NSTXOUT]="NSTXOUT", [NSTVOUT]="NSTVOUT", [NSTFOUT]="NSTFOUT",
+   [NSTEPS]="NSTEPS",
   };
 
 #endif
