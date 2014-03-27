c8c3eba22655657acaf170abc7498f67337df856
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-03-27 11:43:05 -0400

    travis-ci attempt

---
+++ ci/install-gromacs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+
+name=gromacs
+version=4.5.7
+pkg=$name-$version
+
+wget ftp://ftp.gromacs.org/pub/gromacs/$pkg.tar.gz
+tar xf $pkg.tar.gz
+cd $pkg
+./configure --prefix=/usr && make && make install
